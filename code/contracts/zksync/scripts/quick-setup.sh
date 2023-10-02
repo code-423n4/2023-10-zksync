900c75387d2b96de3708f107022a2aee13ff9cc8
vladbochok <vladbochok1@gmail.com>
2023-10-02 18:51:21 +0200

    Fix script

--- code/contracts/zksync/scripts/quick-setup.sh
+++ code/contracts/zksync/scripts/quick-setup.sh
@@ -14,9 +14,9 @@ npm install --global yarn
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+rustup toolchain install nightly
 # install era-test-node
-cargo install --git https://github.com/matter-labs/era-test-node.git --locked --branch boojum-integration
-
+cargo +nightly install --git https://github.com/matter-labs/era-test-node.git --locked --branch boojum-integration
 yarn
 yarn build
 era_test_node run > /dev/null 2>&1 & export TEST_NODE_PID=$!
